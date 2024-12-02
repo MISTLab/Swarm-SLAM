c5105210551b53d5263843dcff46b106a6978745
Anadon <joshua.r.marshall.1991@gmail.com>
2024-12-02 02:40:20 -0500

    Round five of testing and debugging.

--- build-everything.sh
+++ build-everything.sh
@@ -1,28 +1,25 @@
 #! /usr/bin/env bash
 
-
-
-if (( $EUID == 0 )); then
-	add-apt-repository universe
-	apt update
+if (( EUID == 0 )); then
+	SUDO=""
 else
-	sudo add-apt-repository universe
-	sudo apt update
+	SUDO="sudo"
 fi
 
 
-DEPS_0="curl git sudo apt-utils ncurses locales"
+$SUDO add-apt-repository universe
+$SUDO apt update
+
+
+DEPS_0="curl git sudo apt-utils ncurses-term locales"
 DEPS_1="libboost-all-dev cmake libtbb-dev  intel-mkl-full python3-rosdep python3-colcon-common-extensions sudo git software-properties-common python3-dev python3-full python3-pip"
-DEPS_2="ros-dev-tools ros-jazzy-desktop ros-jazzy-rtabmap ros-jazzy-rtabmap-msgs ros-jazzy-navigation2 ros-jazzy-nav2-bringup  ros-jazzy-nav2-minimal-tb* ros-jazzy-perception-pcl ros-jazzy-rtabmap-conversions ros-rolling-cv-bridge " 
+DEPS_2="ros-dev-tools ros-jazzy-desktop ros-jazzy-rtabmap ros-jazzy-rtabmap-msgs ros-jazzy-navigation2 ros-jazzy-nav2-bringup  ros-jazzy-nav2-minimal-tb* ros-jazzy-perception-pcl ros-jazzy-rtabmap-conversions ros-rolling-cv-bridge "
 
-if (( $EUID == 0 )); then
-	apt install -y $DEPS_0
-else
-	sudo apt install -y $DEPS_0
-fi
+$SUDO apt install -y "$DEPS_0"
 
-sudo locale-gen en_US en_US.UTF-8
-sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+
+$SUDO locale-gen en_US en_US.UTF-8
+$SUDO update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
 export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 
@@ -31,20 +28,15 @@ git clone https://github.com/lajoiepy/cslam_interfaces.git
 git clone https://github.com/lajoiepy/cslam_experiments.git 
 git clone https://github.com/borglab/gtsam.git 
 
-if (( $EUID == 0 )); then
-	apt install -y $DEPS_1
-else
-	sudo apt install -y $DEPS_1
-fi
+$SUDO apt install -y "$DEPS_1"
 
-
-sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+$SUDO curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo "$UBUNTU_CODENAME") main" | sudo tee "/etc/apt/sources.list.d/ros2.list" > /dev/null
 
 
-sudo apt update
-sudo apt install -y $DEPS_2
+$SUDO apt update
+$SUDO apt install -y "$DEPS_2"
 
 pip install -r requirements.txt
 
@@ -56,7 +48,7 @@ mkdir build
 cd build || exit 1
 cmake ..
  # make check (optional, runs unit tests)
-sudo make install
+$SUDO make install
 
 popd || exit 1
 
@@ -73,7 +65,7 @@ echo "WARNING!!! This is setting up a one-time configuration change because it i
 echo 'source "/opt/ros/jazzy/setup.bash"' >> ~/.bashrc 
 source "/opt/ros/jazzy/setup.bash"
 
-sudo rosdep init
+$SUDO rosdep init
 
 rosdep update
 rosdep install --from-paths src -y --ignore-src --rosdistro jazzy 
@@ -86,4 +78,3 @@ popd || exit 1
 
 colcon build
 colcon test 
-
