3eb7b6cd8350b3be1b784c04d8b4961cf15a4490
Anadon <joshua.r.marshall.1991@gmail.com>
2024-12-01 22:32:13 -0500

    After one round of testing and debugging.

--- build-everything.sh
+++ build-everything.sh
@@ -1,14 +1,51 @@
 #! /usr/bin/env bash
 
+
+
+if (( $EUID == 0 )); then
+	add-apt-repository universe
+	apt update
+else
+	sudo add-apt-repository universe
+	sudo apt update
+fi
+
+
+DEPS_0="curl git sudo"
+DEPS_1="libboost-all-dev cmake libtbb-dev  intel-mkl-full python3-rosdep python3-colcon-common-extensions sudo git software-properties-common locales python3-dev python3-full python3-pip"
+DEPS_2="ros-dev-tools ros-jazzy-desktop"
+
+if (( $EUID == 0 )); then
+	apt install -y $DEPS_0
+else
+	sudo apt install -y $DEPS_0
+fi
+
 git clone https://github.com/lajoiepy/cslam.git 
 git clone https://github.com/lajoiepy/cslam_interfaces.git 
 git clone https://github.com/lajoiepy/cslam_experiments.git 
+git clone https://github.com/borglab/gtsam.git 
+
+if (( $EUID == 0 )); then
+	apt install -y $DEPS_1
+else
+	sudo apt install -y $DEPS_1
+fi
+
+sudo locale-gen en_US en_US.UTF-8
+sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+export LANG=en_US.UTF-8
+
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo "$UBUNTU_CODENAME") main" | sudo tee "/etc/apt/sources.list.d/ros2.list" > /dev/null
+
+
+sudo apt update
+sudo apt install $DEPS_2
 
 pip install -r requirements.txt
 
-sudo apt-get install libboost-all-dev cmake libtbb-dev  intel-mkl-full python3-rosdep python3-colcon-common-extensions
-
-git clone git@github.com:borglab/gtsam.git 
 
 pushd gtsam || exit 1
 
@@ -26,30 +63,6 @@ popd || exit 1
 ##################
 
 echo "WARNING!!! This deviates from Swarm-Slam instructions by using ROS 2 Jazzy instead of Foxy"
-
-locale  # check for UTF-8
-
-sudo apt update && sudo apt install locales
-sudo locale-gen en_US en_US.UTF-8
-sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
-export LANG=en_US.UTF-8
-
-locale  # verify settings
-
-sudo apt install software-properties-common
-sudo add-apt-repository universe
-
-sudo apt update && sudo apt install curl -y
-sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
-
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo "$UBUNTU_CODENAME") main" | sudo tee "/etc/apt/sources.list.d/ros2.list" > /dev/null
-
-sudo apt update && sudo apt install ros-dev-tools
-
-sudo apt update 
-
-sudo apt install ros-jazzy-desktop
-
 echo "WARNING!!! This is setting up a one-time configuration change because it is destructive to your environment.  This installer is REQUIRING that you take manual action to make this change permenent."
 
 # Replace ".bash" with your shell if you're not using bash
@@ -62,4 +75,4 @@ sudo rosdep init
 rosdep update
 rosdep install --from-paths src -y --ignore-src --rosdistro jazzy 
 
-colcon test 
+colcon test
